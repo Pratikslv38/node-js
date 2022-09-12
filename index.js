3da64640e9bd0ed04e668a0a6325956cacdbec63
root <root@ip-172-31-87-155.ec2.internal>
2022-09-12 10:22:36 +0000

    update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end("Welcome to Devops");
 });
 
-server.listen(8000, "34.238.250.60", () =>{
+server.listen(8000, "34.238.250.60", () => {
   console.log("listening to the port no 8000"); 
 }); 
